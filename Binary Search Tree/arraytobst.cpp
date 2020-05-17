bfdea9614e70858660b4f89cd42573af74d1b396
abhayrpatel10 <abhayrpatel10@gmail.com>
2020-05-17 13:45:43 +0530

    bst problem

---
+++ Binary Search Tree/arraytobst.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode *construct(vector<int> &nums,int s,int e){
+            if(s>e){
+                return NULL;
+            }
+            
+            int mid=(s+e)/2;
+            TreeNode *root=new TreeNode(nums[mid]);
+            root->left=construct(nums,s,mid-1);
+            root->right=construct(nums,mid+1,e);
+            return root;
+            
+        
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return construct(nums,0,nums.size()-1);
+    }
+};
