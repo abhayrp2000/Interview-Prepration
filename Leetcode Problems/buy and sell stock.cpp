4c2ec3dd03f25946f45fb079eacb7873a45178aa
abhayrpatel10 <abhayrpatel10@gmail.com>
2020-05-27 21:15:51 +0530

    changes

--- Leetcode Problems/buy and sell stock.cpp
+++ Leetcode Problems/buy and sell stock.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxProfit = 0;
-        int n=prices.size();
-        for(int i = 1; i < n; i++) {
-            int j = i - 1;
-            
-            if(prices[i] - prices[j]>0) {
-                maxProfit += (prices[i] - prices[j]);
+        
+        int maxP=0;
+        
+        for(int i=1;i<prices.size();i++){
+            for(int j=0;j<i;j++){
+                if(prices[j]<prices[i]){
+                    maxP=max(prices[i]-prices[j],maxP);
+                }
             }
         }
         
-        return maxProfit;
+        return maxP;
+    
         
     }
 };
