3df5490e5d5a0bbd0cbd22970b3f32d167ca4ba6
abhayrpatel10 <abhayrpatel10@gmail.com>
2020-06-13 22:00:51 +0530

    heap push and pop

--- Heap/basics.cpp
+++ Heap/basics.cpp
@@ -3,11 +3,9 @@
 
 using namespace std;
 
-class Heap{`
+class Heap{
     vector<int> v;
     bool minType;//min or max heap
-
-    public:
 
     bool compare(int a,int b){
         if(minType){
@@ -16,7 +14,32 @@ class Heap{`
             return a>b;
         }
     }
-    Heap(int size=10,int type){
+
+    void heapify(int index){
+        int left=2*index;
+        int right=left+1;
+
+        int last=v.size()-1;
+        int min_index=index;
+
+        if(left<=last && compare(v[left],v[index])){
+            min_index=left;
+        }
+
+        if(right<=last && compare(v[right],v[min_index])){
+            min_index=right;
+        }
+
+        if(min_index!=index){
+            swap(v[index],v[min_index]);
+            heapify(min_index);
+        }
+    }
+
+    public:
+
+    
+    Heap(int size=10,int type=true){
         v.reserve(size);
         v.push_back(-1);
         minType=type;
@@ -35,9 +58,39 @@ class Heap{`
             parent=parent/2;
         }
     }
-}
+
+    int top(){
+        return v[1];
+    }
+
+    void pop(){
+        int last_index=v.size()-1;
+        swap(v[1],v[last_index]);
+        v.pop_back();
+
+        heapify(1);
+    }
+
+    bool isEmpty(){
+        return v.size()==1;
+    }
+};
 
 
 int main(){
 
+    Heap h;
+    int n=10;
+
+    for(int i=0;i<n;i++){
+        h.push(i);
+    }
+
+    while(!h.isEmpty()){
+        cout<<h.top()<<endl;
+        h.pop();
+    }
+
+
+    return 0;
 }
