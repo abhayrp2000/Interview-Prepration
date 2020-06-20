eb7b275b6f833685cd0770e1d5460f84b220f56c
abhayrpatel10 <abhayrpatel10@gmail.com>
2020-06-20 12:28:49 +0530

    dfs added

---
+++ Graph/dfs.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<list>
+#include<map>
+
+using namespace std;
+
+class Graph{
+   
+    map<int,list<int> > l;
+    public:
+    
+
+    void addEdge(int x,int y){
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
+
+    void dfs_helper(int src,map<int,bool> &visited){
+        cout<<src<<" ";
+        visited[src]=true;
+
+        for(auto val:l[src]){
+            if(!visited[val]){
+                dfs_helper(val,visited);
+            }
+        }
+    }
+
+    void dfs(int src){
+
+        map<int,bool> visited;
+        for(auto p: l){
+            visited[p.first]=false;
+        }
+
+        dfs_helper(src,visited);
+    }
+
+   
+
+    
+
+
+};
+
+int main(){
+
+    Graph g;
+
+    g.addEdge(0,1);
+    g.addEdge(0,3);
+    g.addEdge(1,2);
+    g.addEdge(2,3);
+    g.addEdge(3,4);
+    g.addEdge(4,5);
+
+
+    g.dfs(0);
+    
+
+    
+    return 0;
+}
