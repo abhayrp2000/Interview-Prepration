61a458b92aee2ddfff1a0e32365bc47d0af3d50c
abhayrpatel10 <abhayrpatel10@gmail.com>
2020-07-21 12:24:38 +0530

    prob added

---
+++ Greedy Algorithms/jump_game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int last=nums.size()-1;
+        int i;
+        for(i=nums.size()-1;i>=0;i--){
+            if(i+nums[i]>=last){
+                last=i;
+            }
+        }
+        
+        if(last==0){
+            return true;
+        }
+        
+        return false;
+        
+    }
+};
